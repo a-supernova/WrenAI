632a5d0f32c9990b4c8e6ddea75cf2dbcfbb8edf
Marcelo M. Fleury <marcelomf@gmail.com>
2025-09-09 15:04:50 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz . --exclude .git --exclude node_modules --exclude .env --exclude .env.local --exclude .next ubuntu@cashmind.asupernova.com.br:/opt/WrenAI
+rsync -avz . --exclude .git --exclude node_modules --exclude .env --exclude .env.local --exclude .next ubuntu@54.196.19.197:/opt/WrenAI
